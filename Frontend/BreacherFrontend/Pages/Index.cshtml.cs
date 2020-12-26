53964bd13bbcad060fce6c716ce9f882fd9407b7
magico13 <magico1313@gmail.com>
2020-12-26 14:00:33 -0500

    Styling and cleanup. Docker compose support

--- Frontend/BreacherFrontend/Pages/Index.cshtml.cs
+++ Frontend/BreacherFrontend/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.IO;
@@ -14,17 +15,20 @@ namespace BreacherFrontend.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly IHttpClientFactory _clientFactory;
         private readonly BreachCache _cache;
+        private readonly IConfiguration _config;
 
         public IFormFile FormFile { get; set; }
         public BreachResponse BreachData { get; set; }
 
         public string Sequence { get; set; }
+        public string Targets { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory clientFactory, BreachCache cache)
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory clientFactory, BreachCache cache, IConfiguration config)
         {
             _logger = logger;
             _clientFactory = clientFactory;
             _cache = cache;
+            _config = config;
         }
 
         public void OnGet(string cacheKey)
@@ -37,6 +41,11 @@ namespace BreacherFrontend.Pages
                 {
                     BreachData = breachData;
                     Sequence = string.Join(" ", breachData.SequenceText);
+                    Targets = "";
+                    foreach (var seq in breachData.Targets)
+                    {
+                        Targets += string.Join(" ", seq) + "<br />";
+                    }
                     _logger.LogInformation(Sequence);
                 }
             }
@@ -45,8 +54,9 @@ namespace BreacherFrontend.Pages
         public async Task<IActionResult> OnPostUploadAsync()
         {
             string key = null;
-            if (FormFile.Length > 0)
+            if (FormFile?.Length > 0)
             {
+                string url = _config["BackendURL"] + "/breach";
                 HttpClient client = _clientFactory.CreateClient();
                 using MemoryStream stream = new MemoryStream();
                 await FormFile.CopyToAsync(stream);
@@ -55,7 +65,7 @@ namespace BreacherFrontend.Pages
                 MultipartFormDataContent formData = new MultipartFormDataContent();
                 formData.Add(new StreamContent(stream), "file", FormFile.FileName);
 
-                HttpResponseMessage response = await client.PostAsync("http://localhost:5002/breach", formData);
+                HttpResponseMessage response = await client.PostAsync(url, formData);
                 response.EnsureSuccessStatusCode();
 
                 string content = await response.Content.ReadAsStringAsync();
